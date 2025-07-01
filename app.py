dfc0033723ebf3b4bef413ba541673328d520057
DozKooki <dishankshetty29@gmail.com>
2025-07-01 14:30:45 +0000

    Release test

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ def create_app():
     return app
 
 if __name__ == "__main__":
+    from app import create_app
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
